89d43377992839a2bcb01b52e46e3648a9a3665f
amjadsalameh <amjad.salameh@gmail.com>
2022-08-20 17:36:06 +0300

    update file and web home page

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var http = require('http');
 var hd= require('./myfuncs');
 var url= require('url');
+var web= require("./files");
 
 const hostname='127.0.0.1';
 const port=8080;
 
 const server = http.createServer((req,res)=>{
   var from = req.headers.from;
- 
+  var q = url.parse(req.url,true);
+  var file = q.path.substring(1);
+  if(file){
+    web.myweb(file,req,res);
+  }
+  else{
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("<html><head><title></title></head><body>");
+    res.end(
+      '</br>host: ' + q.host +
+    '</br>hostname: ' + q.hostname +
+    '</br>href: ' + q.href +
+    '</br>path: ' + q.path +
+    //'</br>query: ' + q.query.toString() +
+    '</br>' + hd.hDate() +
+    "</body></html>");
+  }
   /* 
   res.statusCode = 200;
   res.setHeader('Content-Type','text/html');
   */
-  //or 
-  res.writeHead(200,{"Content-Type":"text/html"});
-  var q=url.parse(req.url,true).query;
-  var txt = q.year + " " + q.month;
-  res.write("<html><head><title>"+txt+"</title></head><body>");
-  res.end('Custom Server! </br>' + req.url +"</br>"+
-   hd.hDate() +
-  "</body></html>");
 }).listen(port,hostname,(p)=>{
     console.log("Server Started"); 
 });
---
+++ files.js
@@ -0,0 +1,20 @@
+const files = require("fs");
+exports.myweb = (index, req,res)=>{
+    if(index=="index.html" || index=="index"||index=="default" || index=="home"){
+        files.readFile("index.html",(err,data)=>{
+            if(err){
+                res.writeHead(300,{"Content-Type":"text/html"});
+                return res.end("cannot find resource!"); 
+            }
+            else{
+                res.writeHead(200,{"Content-Type":"text/html"});
+                res.write(data);
+                return res.end();
+            }
+        });
+    }
+    else{
+        res.writeHead(300,{"Content-Type":"text/html"});
+        return res.end("cannot find resource!"); 
+    }
+};
